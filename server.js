086584d584323cbc799324afbebacbb570ad8b59
A SHIVA KUMAR <shivuas605@gmail.com>
2025-10-13 19:55:23 +0530

    third commit

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/',(req, res) => {
+    res.send('hello world')
+});
+
 app.listen(5000, () => {
     console.log('server is running');
 });
