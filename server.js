a06978ecca721525aa8946f007ed023470938500
A SHIVA KUMAR <shivuas605@gmail.com>
2025-10-27 19:38:44 +0530

    sixth commit

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ app.get('/',(req, res) => {
     res.send('hello world, i am shivu from halehalli')
 });
 app.get('/products', (req, res)=>{
-    res.send('hello this is products')
+    res.json({message: 'this is an products get route', page:5})
 });
-
+app.post('/products', (req, res)=>{
+     res.json({message:'this is an product post route'})
+})
+app.put('/products', (req, res)=>{
+     res.json({message:'this is an product put route'})
+})
 app.listen(5000, () => {
     console.log('server is running');
 });
