7261463a6b9b78c0a1eb6fe19e422c3ba41f4a0e
A SHIVA KUMAR <shivuas605@gmail.com>
2025-10-27 18:52:34 +0530

    fifth commit

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express();
 app.get('/',(req, res) => {
     res.send('hello world, i am shivu from halehalli')
 });
+app.get('/products', (req, res)=>{
+    res.send('hello this is products')
+});
 
 app.listen(5000, () => {
     console.log('server is running');
